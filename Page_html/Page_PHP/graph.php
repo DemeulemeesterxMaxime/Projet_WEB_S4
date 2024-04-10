a7500daa4087a266da6d7ef46e266349beb3a9ee
Jérémy LAMOUR <jeremy.lamour@student.junia.com>
2024-04-10 15:33:55 +0200

    modif pour les graphes

--- Page_html/Page_PHP/graph.php
+++ Page_html/Page_PHP/graph.php
@@ -10,11 +10,13 @@
         } else {
             echo ("<link rel='stylesheet' href='../../public/Css_Pages/root.css'>");
             echo ("<link rel='stylesheet' href='../../public/Css_Pages/accueil.css'>");
+            echo ("<link rel='stylesheet' href='../../public/Css_Pages/graph.css'>");
         }
     } else {
     ?>
         <link rel="stylesheet" href="../../public/Css_Pages/root.css">
         <link rel="stylesheet" href="../../public/Css_Pages/accueil.css">
+        <link rel="stylesheet" href="../../public/Css_Pages/graph.css">
     <?php } ?>
 
     <!--Lien permettant d'accéder au code CSS-->
@@ -33,13 +35,40 @@
     if (empty($_SESSION["id"])) {
     header("Location: sign_in_up.php");
     } ?>
-    <div class="graphique">
-        <h2 class="h2-titre-acc">Découvre tes moyennes dans les différents modules </h2>
-        <div class="graph">
-            <canvas id="myChart"></canvas>
-        </div>
+<div class="graphique">
+    <h2 class="h2-titre-acc">Découvre tes moyennes dans les différents modules</h2>
+
+    <div id="requete">
+        <?php 
+        try {
+            $id = $_SESSION['id'];
+            require_once("../../SQL_Traitement/fonctions.php");
+            require_once("../../SQL_Traitement/DB_connect.php");
+            $req = "SELECT * FROM eleve_module WHERE id_eleve=:id AND moyenne_module IS NOT NULL"; // Correction : Utiliser IS NOT NULL pour tester la nullité
+            $reqPrep = $conn->prepare($req);
+            $reqPrep->execute(array(':id' => $id));
+            $resultat = $reqPrep->fetchAll(PDO::FETCH_ASSOC);
+            // Convertir en JSON
+            $json = json_encode($resultat);
+            
+            // Afficher le JSON
+            ?><p id="para_requete" style="display: none;"><?php echo $json ?></p><?php // Correction : Utiliser display: none; pour cacher l'élément
+            $conn = NULL; // On ferme la connexion
+        } catch (Exception $e) {
+            die("Erreur : " . $e->getMessage());
+        }
+        ?>
+    </div>    
+
+    <div class="graph"><canvas id="linec" aria-label="chart" role="img"></canvas></div>
+    
+    <div id="matières">
+        <h2>Découvre tes moyennes dans les différentes matières</h2>
+        
     </div>
+</div>
+
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <script src="../../public/Js_Pages/graph.js"></script>
+    <script src="../../public/JS/graph.js"></script>
     <?php include_once("../Page_Structuration/footer.php"); ?>
 </body>
